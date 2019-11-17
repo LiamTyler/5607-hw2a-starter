7ffcd13a16c54b35ade490697453c56bcf5a3768
Liam Tyler <tyler.liam7@gmail.com>
2019-11-17 13:24:27 -0600

    adding hw2b starter template

---
+++ src/vshader2b.glsl
@@ -0,0 +1,11 @@
+// vertex shader that modifies the vertex position
+#version 150
+in vec4 vertex_position;
+in vec4 vertex_color;
+out vec4 vcolor;
+uniform mat4 M;
+
+void main()  {
+	gl_Position = M*vertex_position; 
+	vcolor = vertex_color;
+}//
